917e392084b09e479de865ed821bd716483788f1
l4u532 <kl@shipbeat.com>
2016-08-10 12:15:59 +0200

    IP works almost

---
+++ FG_landing-page.js
@@ -0,0 +1,42 @@
+// clear local storage
+localStorage.clear();
+
+// make cookie sharable across domains
+var trackAcross = ";domain=" + location.host.replace('www.','') + "; path=/";
+
+// log input of current field to cookie when user changes field
+$("form input:text").focusout(function () {
+    localStorage[$(this).attr('name')] = $(this).val();
+    document.cookie = $(this).attr('name') + "=" + $(this).val() + trackAcross;
+
+    // create cookie & start FraudGuard once all fields have been provided
+    if (localStorage.getItem("firstName") &&
+        localStorage.getItem("lastName") &&
+        localStorage.getItem("invoiceAddress") &&
+        localStorage.getItem("invoiceZipcode") &&
+        localStorage.getItem("invoiceCity")) {
+        // userID = unique user identifier, created with full name + address
+        var userID =    localStorage.getItem("firstName") + " " +
+                        localStorage.getItem("lastName")  + " " +
+                        localStorage.getItem("invoiceAddress");
+
+        document.cookie = "userID=" + userID + trackAcross;
+
+        // start FraudGuard
+        var _fg = window._fg = window._fg || {};
+        _fg['_setAccountId'] = '42c2225b-d170-47d2-ad03-16f8c8ef5c92';
+        _fg['_setUserId'] = userID;
+        _fg['_setSessionId'] = "";
+        _fg['_setTrack'] = 'page_view';
+
+        (function(d, s, id) {
+            var e, f = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            e = d.createElement(s); e.id = id;
+            e.src = '//fraud.aletia.io/snippet/s.js',
+                f.parentNode.insertBefore(e, f);
+        })(document, 'script', 'fg-beacon');
+
+        console.log("This userID was sent to FraudGuard: " + userID);
+    }
+});
--- ReadURL.js
+++ ReadURL.js
@@ -9,3 +9,14 @@
 // charge=0.00
 // discount=0.00
 // subscriptions=1
+
+// Get user's ip address
+var currentIP;
+
+$(function() {
+    $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
+        function(json) {
+            currentIP = json.ip;
+            return currentIP;
+        }
+    );
